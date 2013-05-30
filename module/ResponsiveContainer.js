812acf5926cec719651aad800a282d625b73e749
unknown <clement.mathieu@outlook.com>
2013-05-30 16:16:55 +0200

    align multiPanes syntax on sidePane

--- module/ResponsiveContainer.js
+++ module/ResponsiveContainer.js
@@ -44,13 +44,15 @@ define([
 		DEPTH_ATTRIBUTE_NAME: "dojo-depth",
 		
 		// MultiPanes variables
-		_multiPanes: null,
-		_MPheader: null,
-		_MPslider: null,
-		_MPmaxPanes: 3,
-		_MPcurrentDepth: 0,
-		_MPdepths: ["root"], //dummy element to start the index at 1
-		
+		_multiPanes: {
+			domNode: null,
+			header: null,
+			slider: null,
+			maxPanes: 3,
+			currentDepth: 0,
+			depths: ["root"], //dummy element to start the index at 1
+		},
+
 		// SidePane 
 		_sidePane: {
 			domNode: null,
@@ -79,7 +81,7 @@ define([
 					domClass.add(children[i], "view");
 					domClass.add(children[i], "mblBackground");
 				}else if(children[i].nodeName === "H1"){
-					this._MPheader = children[i];
+					this._multiPanes.header = children[i];
 				}
 			}
 			
@@ -93,14 +95,14 @@ define([
 			
 			// Construct needed markup
 			this._sidePane.domNode = domConstruct.place("<div id='sidePane'></div>", this.domNode, "first");
-			this._multiPanes = domConstruct.place("<div id='multiPanes'></div>", this.domNode, "first");
-			this._MPslider = domConstruct.place("<div id='slider'></div>", this._multiPanes, "first");
-			this._MQDiv = domConstruct.place("<div id='MQmatch'></div>", this._multiPanes, "first");
+			this._multiPanes.domNode = domConstruct.place("<div id='multiPanes'></div>", this.domNode, "first");
+			this._multiPanes.slider = domConstruct.place("<div id='slider'></div>", this._multiPanes.domNode, "first");
+			this._MQDiv = domConstruct.place("<div id='MQmatch'></div>", this._multiPanes.domNode, "first");
 
-			if(this._MPheader){
-				domConstruct.place(this._MPheader, this._multiPanes, "first");
-				this._MPslider.style.marginTop="-"+this._MPheader.offsetHeight+"px";
-				this._MPslider.style.paddingTop=""+this._MPheader.offsetHeight+"px";
+			if(this._multiPanes.header){
+				domConstruct.place(this._multiPanes.header, this._multiPanes.domNode, "first");
+				this._multiPanes.slider.style.marginTop="-"+this._multiPanes.header.offsetHeight+"px";
+				this._multiPanes.slider.style.paddingTop=""+this._multiPanes.header.offsetHeight+"px";
 			}
 			
 			// dojoDisplay event should have a detail.view string property containing the id of the view to display
@@ -117,9 +119,9 @@ define([
 		
 		_getCurrentMaxPanes: function(){
 			switch(this._MQDiv.offsetWidth){
-				case 1: return this._MPmaxPanes; break;
-				case 2: return Math.min(this._MPmaxPanes, 2); break;
-				case 3: return Math.min(this._MPmaxPanes, 1); break;
+				case 1: return this._multiPanes.maxPanes; break;
+				case 2: return Math.min(this._multiPanes.maxPanes, 2); break;
+				case 3: return Math.min(this._multiPanes.maxPanes, 1); break;
 			}
 		},
 		
@@ -160,54 +162,54 @@ define([
 				var currentMaxPanes = this._getCurrentMaxPanes();
 				
 				//The view is already display so we shake it so the user can see it
-				if(depth === this._MPcurrentDepth && viewId === this._MPdepths[depth]){
+				if(depth === this._multiPanes.currentDepth && viewId === this._multiPanes.depths[depth]){
 					this._startTransition(viewDomNode, "shake");
 			
-				}else if(depth === this._MPcurrentDepth && viewId !== this._MPdepths[depth]){ //a view is already displayed at current depth so just update the view
+				}else if(depth === this._multiPanes.currentDepth && viewId !== this._multiPanes.depths[depth]){ //a view is already displayed at current depth so just update the view
 					if(animate){
-						this._startTransition(viewDomNode, "inPlaceTransition", {oldNode: dom.byId(this._MPdepths[this._MPcurrentDepth])});
+						this._startTransition(viewDomNode, "inPlaceTransition", {oldNode: dom.byId(this._multiPanes.depths[this._multiPanes.currentDepth])});
 					}else{
-						this._MPslider.appendChild(viewDomNode);
-						domConstruct.place(this._MPdepths[this._MPcurrentDepth], this.domNode, "last"); 
+						this._multiPanes.slider.appendChild(viewDomNode);
+						domConstruct.place(this._multiPanes.depths[this._multiPanes.currentDepth], this.domNode, "last"); 
 					}
-					this._MPdepths[depth] = viewId;			
+					this._multiPanes.depths[depth] = viewId;			
 		
-				}else if(this._MPcurrentDepth === (depth-1)){ //add a new depth level 
-					this._MPcurrentDepth++;
-					this._MPdepths.push(viewId); 
-					this._MPslider.appendChild(viewDomNode);
+				}else if(this._multiPanes.currentDepth === (depth-1)){ //add a new depth level 
+					this._multiPanes.currentDepth++;
+					this._multiPanes.depths.push(viewId); 
+					this._multiPanes.slider.appendChild(viewDomNode);
 						
 					//Slide transition
-					if(animate && (this._MPcurrentDepth > currentMaxPanes)){
-						this._startTransition(this._MPslider, "slideTransition");
-					}else if(this._MPcurrentDepth > currentMaxPanes){
-						this._cbMap["slideTransition"].end(this._MPslider, "slideTransition");
+					if(animate && (this._multiPanes.currentDepth > currentMaxPanes)){
+						this._startTransition(this._multiPanes.slider, "slideTransition");
+					}else if(this._multiPanes.currentDepth > currentMaxPanes){
+						this._cbMap["slideTransition"].end(this._multiPanes.slider, "slideTransition");
 					}
 					// Compression transition
-					if(animate && (this._MPcurrentDepth <= currentMaxPanes)){ 
-						this._startTransition(this._MPslider, "widthTransition"); //the transition is listening for the new class colX
+					if(animate && (this._multiPanes.currentDepth <= currentMaxPanes)){ 
+						this._startTransition(this._multiPanes.slider, "widthTransition"); //the transition is listening for the new class colX
 					}
 					
-					domClass.add(this._MPslider, "col"+Math.min(this._MPmaxPanes,this._MPcurrentDepth));
+					domClass.add(this._multiPanes.slider, "col"+Math.min(this._multiPanes.maxPanes,this._multiPanes.currentDepth));
 		
-				}else if(this._MPcurrentDepth > depth){ //remove depth level
-					var step = this._MPcurrentDepth - depth;
+				}else if(this._multiPanes.currentDepth > depth){ //remove depth level
+					var step = this._multiPanes.currentDepth - depth;
 					var i = 0;
 					for(i = 0; i <= step; i++){
-						domConstruct.place(dom.byId(this._MPdepths.pop()), this.domNode, "last");
+						domConstruct.place(dom.byId(this._multiPanes.depths.pop()), this.domNode, "last");
 					}
-					this._MPdepths[depth] = viewId;
-					for(i = depth; i > (depth-this._MPmaxPanes) && i > 0; i-- ){
-						domConstruct.place(dom.byId(this._MPdepths[i]), this._MPslider, "first");
+					this._multiPanes.depths[depth] = viewId;
+					for(i = depth; i > (depth-this._multiPanes.maxPanes) && i > 0; i-- ){
+						domConstruct.place(dom.byId(this._multiPanes.depths[i]), this._multiPanes.slider, "first");
 					}
-					this._MPcurrentDepth = depth;
+					this._multiPanes.currentDepth = depth;
 					
-					if(animate && (this._MPcurrentDepth <= currentMaxPanes)){ // expansion transition
-						this._startTransition(this._MPslider, "widthTransition"); //the transition is listening for the new class colX
+					if(animate && (this._multiPanes.currentDepth <= currentMaxPanes)){ // expansion transition
+						this._startTransition(this._multiPanes.slider, "widthTransition"); //the transition is listening for the new class colX
 					}
 					
-					for(i = this._MPmaxPanes; i > this._MPcurrentDepth; i--){
-						domClass.remove(this._MPslider, "col"+i);
+					for(i = this._multiPanes.maxPanes; i > this._multiPanes.currentDepth; i--){
+						domClass.remove(this._multiPanes.slider, "col"+i);
 					}
 				}
 			}else if(type[0] === "sidePane"){
@@ -218,13 +220,13 @@ define([
 					this._sidePane.domNode.style.display = "block";
 					
 					if(animate && this._getSideTransition() === "under"){
-						this._startTransition(this._multiPanes, "sideTransition");
+						this._startTransition(this._multiPanes.domNode, "sideTransition");
 					}else if(animate && this._getSideTransition() === "over"){
 						this._startTransition(this._sidePane.domNode, "sideSlideTransition");
 					}
 					
 					domClass.add(this._sidePane.domNode, side);
-					domClass.add(this._multiPanes, "overlayed "+side);
+					domClass.add(this._multiPanes.domNode, "overlayed "+side);
 					
 					var eventName = has("touch") ? "touchstart" : "mousedown"; //Those are the event used by dojo so we have to use them if we went to prevent the default action
 					var handler = capture(win.body(), eventName, lang.hitch(this, function(event){
@@ -250,7 +252,7 @@ define([
 			this._sidePane.visible = false;
 			this._sidePane.domNode.style.display = "none";
 			domClass.remove(this._sidePane.domNode, this._sidePane.side);
-			domClass.remove(this._multiPanes, "overlayed "+this._sidePane.side);
+			domClass.remove(this._multiPanes.domNode, "overlayed "+this._sidePane.side);
 			domConstruct.place(this._sidePane.domNode.children[0], this.domNode, "last");
 		},
 		
@@ -349,7 +351,7 @@ define([
 
 		_slideTransitionEnd: function(node, transition){
 			this._baseTransitionEnd(node, transition);
-			if(this._MPslider.children.length > this._MPmaxPanes){
+			if(this._multiPanes.slider.children.length > this._multiPanes.maxPanes){
 				domConstruct.place(query("#slider>.view:first-child")[0], this.domNode, "last"); 
 			}
 		}
